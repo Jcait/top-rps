5506e53486466893d271eae4d43d2adfc565118c
Jo <134626490+Jcait@users.noreply.github.com>
2024-01-05 14:42:00 +0000

    add for loop for game function

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ console.log("Hello World")
 // CHoices are ROCK, PAPER, or SCISSORS
 // The choices must be made randomly 
 
-function getComputerChoice () {
-    // use math to deice the which chouice is call
-    //  0-99 to give each choice a 1/3rd chance
-    let choice = Math.floor(Math.random() * 100);
-    if( choice <= 33) {
-        return 'rock'
-    }
-    else if (choice > 33 && choice <= 66) {
-        return 'paper'
-    } 
-    else {
-        return "scissors"
-    }
-    return choice
-}
+// function getComputerChoice () {
+//     // use math to deice the which chouice is call
+//     //  0-99 to give each choice a 1/3rd chance
+//     let choice = Math.floor(Math.random() * 100);
+//     if( choice <= 33) {
+//         return 'rock'
+//     }
+//     else if (choice > 33 && choice <= 66) {
+//         return 'paper'
+//     } 
+//     else {
+//         return "scissors"
+//     }
+//     return choice
+// }
 
 // using ASCII value to simplyfy conditionals
 // rock > paper true
@@ -40,35 +40,62 @@ function getComputerChoice () {
 // const computerSelection = getComputerChoice()
 
 function game() {
+
+
+    function getComputerChoice () {
+        // use math to deice the which chouice is call
+        //  0-99 to give each choice a 1/3rd chance
+        let choice = Math.floor(Math.random() * 100);
+        if( choice <= 33) {
+            return 'rock'
+        }
+        else if (choice > 33 && choice <= 66) {
+            return 'paper'
+        } 
+        else {
+            return "scissors"
+        }
+        return choice
+    }
+
+
     let playerScore = 0;
     let computerScore = 0
-    function playRound(playerSelection, computerSelection) {
-        if(playerSelection == computerSelection) {
-            return 'Tie Game!!"'
+    // Loops though 5 games
+    for(i = 0; i < 6; i++) {
+        let playerSelection = prompt().toLocaleLowerCase()
+        let computerSelection = getComputerChoice()
+        // Calls the player and computer to make their choice
+        // ASCII values and score added if the player or computer win
+        // May make it first to five in future
+        function playRound(playerSelection, computerSelection) {
+            if(playerSelection == computerSelection) {
+                return 'Tie Game!!"'
+            }
+            else if (playerSelection > computerSelection) {
+                playerScore ++
+                return `You selected ${playerSelection} and the computer selected ${computerSelection}, congrats you win!`
+            } else {
+                computerScore++
+                return `You selected ${playerSelection} and the computer selected ${computerSelection}, you lose!!`
+            }
+            
         }
-        else if (playerSelection > computerSelection) {
-            playerScore ++
-            return `You selected ${playerSelection} and the computer selected ${computerSelection}, congrats you win!`
-        } else {
-            computerScore++
-            return `You selected ${playerSelection} and the computer selected ${computerSelection}, you lose!!`
-        }
-        
+        // Checks the result of each round for testing purposes
+        console.log(playRound(playerSelection, computerSelection))
+        console.log(playerSelection)
+        console.log(computerSelection)
     }
+
     
-    let playerSelection = prompt().toLocaleLowerCase()
-    let computerSelection = getComputerChoice()
 
-    console.log(playRound(playerSelection, computerSelection))
-    console.log(playerSelection)
-    console.log(computerSelection)
-
+// Scpre calculating at the end
     if(playerScore > computerScore) {
         return `Great Job you scored ${playerScore} while the computer only scored ${computerScore}. A winner is you!`
     } else if (playerScore < computerScore)  {
         return `Tough break,  you scored ${playerScore} while the computer scored ${computerScore} you lose. `
     } else {
-        return ' You tied with the computer'
+        return ' You'
     }
 
 }
